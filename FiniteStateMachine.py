68005ea98902ed5f50d1d1ff1471690fd5786423
Nathaniel <njfernandes24@tamu.edu>
2023-04-19 20:40:22 -0500

    made instructions into an enum to help prevent bugs

--- FiniteStateMachine.py
+++ FiniteStateMachine.py
@@ -33,31 +33,31 @@ CABINET_STATE = {
     # reset to initial
     STATES.reset: {
         "instructions": [
-            ["reset_values"],
-            ["setup_reader"]
+            [INSTRUCTIONS.reset_values],
+            [INSTRUCTIONS.setup_reader]
         ],
     },
 
     # drawer is closed - waiting for open
     STATES.drawer_closed: {
         "instructions": [
-            ["check_if_door_is_closed"],
-            ["send_drawer_closed_status_event"],
-            ["check_if_scanned"],
+            [INSTRUCTIONS.check_if_door_is_closed],
+            [INSTRUCTIONS.send_drawer_closed_status_event],
+            [INSTRUCTIONS.check_if_scanned],
         ]
     },
 
     # Cabinet drawer is open
     STATES.drawer_open: {
         "instructions": [
-            ["send_drawer_opened_status_event"],
+            [INSTRUCTIONS.send_drawer_opened_status_event],
         ]
     },
 
     # UHF Reader is scanning
     STATES.scanning: {
         "instructions": [
-            ["run_scan"]
+            [INSTRUCTIONS.run_scan]
         ]
     }
 }
@@ -71,7 +71,8 @@ def FSM(state: str, execution_context=None):
         This function implements a finite state machine (FSM).
         As with any FSM, there are two parts: the current state and the transition condition.
 
-        param execution_context contains information from the previous state that impacts the execution of this current state 
+        param execution_context contains information from the previous state that impacts the execution of this 
+        current state's instructions 
     '''
     curr_state = CABINET_STATE[state]
     
@@ -97,14 +98,14 @@ def FSM(state: str, execution_context=None):
 # list: [instruction name, [...parameters]]
 def execute(instruction: str, execution_context: str|None):
     ''' Execute the instructions of the FSM'''
-    if instruction == "reset_values":
+    if instruction == INSTRUCTIONS.reset_values:
         try:
             has_scanned = False
             reader.ClosePort()
         except:
             print("Reader already closed")
 
-    elif instruction == "setup_reader":
+    elif instruction == INSTRUCTIONS.setup_reader:
         try:
             reader = helpers.SetupReader()
         except:
@@ -112,12 +113,12 @@ def execute(instruction: str, execution_context: str|None):
 
         return STATES.drawer_closed, CONDITIONS.immediate, None
 
-    elif instruction == "check_if_door_is_closed":
+    elif instruction == INSTRUCTIONS.check_if_door_is_closed:
         # if true, the door started open not closed
         if (GPIO.input(setup.BUTTON)):
             return STATES.drawer_open, CONDITIONS.immediate, EXECUTION_CONTEXT.suppress_sending_event_messages
 
-    elif instruction == "send_drawer_closed_status_event":
+    elif instruction == INSTRUCTIONS.send_drawer_closed_status_event:
         if execution_context == EXECUTION_CONTEXT.suppress_sending_event_messages:
             return
         
@@ -126,20 +127,20 @@ def execute(instruction: str, execution_context: str|None):
                 res = db.table('events').insert({
                     "event": "drawer_close",
                     "cabinet_id": 1,
-                    # "user"
+                    # "user" # TODO: implement authentication - who opened, when, what did they take out?
                 }).execute()
                 print("drawer close?", res)
             except:
                 print("Couldn't send drawer open message")
     
-    elif instruction == "check_if_scanned":
+    elif instruction == INSTRUCTIONS.check_if_scanned:
         if has_scanned:
             return STATES.drawer_open, CONDITIONS.wait_for_door_to_open, None
         
         else:
             return STATES.scanning, CONDITIONS.immediate, None
 
-    elif instruction == "run_scan":
+    elif instruction == INSTRUCTIONS.run_scan:
         if has_scanned:
             return STATES.drawer_closed, CONDITIONS.immediate, EXECUTION_CONTEXT.suppress_sending_event_messages
         
@@ -151,9 +152,12 @@ def execute(instruction: str, execution_context: str|None):
         
         has_scanned=True
     
-    elif instruction == "send_drawer_opened_status_event":
+    elif instruction == INSTRUCTIONS.send_drawer_opened_status_event:
         if execution_context != EXECUTION_CONTEXT.suppress_sending_event_messages:
             try:
+                # reset scan value once the door has been opened
+                has_scanned = False
+
                 res = db.table('events').insert({
                     "event": "drawer_open",
                     "cabinet_id": 1
@@ -164,27 +168,38 @@ def execute(instruction: str, execution_context: str|None):
                 print("Couldn't send drawer open status event")
         
         return STATES.drawer_closed, CONDITIONS.wait_for_door_to_close, None
-
-
+    
+    else:
+        print("did not match any instructions!!!: ", instruction, execution_context)
+        # TODO: this will throw an error, determine best course of action
 
 def isConditionMet(condition, conditionParams):
-    if condition == "immediate":
+    if condition == CONDITIONS.immediate:
         return True
     
-    if condition == "countdown":
-        while updateCounter() > conditionParams:
-            if conditionParams == 0: # hard coded, checks if we're doing state4
-                blink(setup.PED_BLUE, 1, 0.4, 0.4)
-            time.sleep(0.2)
-        return True
-    
-    if condition == "button_press":
-        interrupt()
-        
-        setup.COOLDOWN = time.time()
-        setup.COUNTDOWN_START_TIME = time.time()
+    elif condition == CONDITIONS.wait_for_door_to_open:
+        # TODO: modify interrupt code here
+        input("hit enter when door has opened: ")
 
-        return True
+    elif condition == CONDITIONS.wait_for_door_to_close:
+        # TODO: modify interrupt code here
+        input("hit enter when door has closed: ")
+    
+
+    # if condition == "countdown":
+    #     while updateCounter() > conditionParams:
+    #         if conditionParams == 0: # hard coded, checks if we're doing state4
+    #             blink(setup.PED_BLUE, 1, 0.4, 0.4)
+    #         time.sleep(0.2)
+    #     return True
+    
+    # if condition == "button_press":
+    #     interrupt()
+        
+    #     setup.COOLDOWN = time.time()
+    #     setup.COUNTDOWN_START_TIME = time.time()
+
+    #     return True
     
 def interrupt():
     while True:
